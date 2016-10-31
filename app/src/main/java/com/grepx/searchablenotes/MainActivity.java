afd87d25df8a6fd4445e36026872cefba2ab4027
Greg Pearce <gregrpearce@gmail.com>
2016-10-31 19:49:01 +0000

    Configure so that the normal app is using the pro app content provider with permission.

--- app/src/main/java/com/grepx/searchablenotes/MainActivity.java
+++ app/src/main/java/com/grepx/searchablenotes/MainActivity.java
@@ -68,7 +68,7 @@ public class MainActivity extends AppCompatActivity
   @Override public Loader<Cursor> onCreateLoader(int id, Bundle args) {
     return new CursorLoader(
         getApplicationContext(),
-        new NotesPersistenceContract("com.grepx.searchablenotes.notedata").buildNotesUri(),
+        new NotesPersistenceContract("com.grepx.searchablenotespro.notedata").buildNotesUri(),
         NotesPersistenceContract.NoteEntry.NOTES_COLUMNS, null, null, null
     );
   }
